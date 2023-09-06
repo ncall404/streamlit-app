1d4c3d30e638108d4812eb43bbe4ddcfe936aded
Nathan Call <37190397+ncall404@users.noreply.github.com>
2023-09-06 14:15:48 -0600

    Added tables and graphs/charts.

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
+# Modified by Nathan Call
+for INFO-4120 at UVU.
+
+Original README:
 # TylersDashboard
 dash-boarding in streamlit!!
--- racer_stats.py
+++ racer_stats.py
@@ -4,3 +4,20 @@ st.markdown("# Racer Page 🎈")
 st.sidebar.markdown("# Racer Page 🎈")
 
 st.write(' # Mariokart *Stats Website*')
+
+df_racer = pd.read_csv('data/racer_stats.csv')
+
+#st.write(df_racer)
+
+st.dataframe(df_racer.style
+             .highlight_max(color='green', axis=0, subset=['Speed', 'Acceleration', 'Weight', 'Handling', 'Traction/Grip', 'Mini-Turbo', 'Times First Place', 'Total Races'])
+             .highlight_min(color='red', axis=0, subset=['Speed', 'Acceleration', 'Weight', 'Handling', 'Traction/Grip', 'Mini-Turbo', 'Times First Place', 'Total Races']))
+
+st.line_chart(df_racer, x='Speed', y=['Acceleration', 'Weight', 'Handling', 'Traction/Grip'])
+
+st.header("Racer Speed does not seem to correlate to number of races won.")
+
+x = st.slider('How Many Racers to Show', 1, len(df_racer))
+st.write("Racers by Speed")
+df_fastest_racers = df_racer[['Character', 'Speed']].sort_values("Speed", ascending=False).iloc[0:x]
+st.dataframe(df_fastest_racers)
